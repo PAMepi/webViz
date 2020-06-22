7fd86808e7589d82434c5e3451c08f5a298e8057
rodriguesmsb <rodriguesmsb@gmail.com>
2020-06-21 22:19:58 -0400

    Fix requiriments

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import dash
-import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
 import dash_leaflet as dl
 from dash.dependencies import Output, Input
 from dash_leaflet import express as dlx
